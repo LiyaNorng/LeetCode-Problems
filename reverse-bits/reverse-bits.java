dcbd21187263cfc8201b19d8e334866cba8477df
LiyaNorng <Liya_Norng@yahoo.com>
2021-10-08 21:10:48 -0700

    Time: 1 ms (98.80%), Space: 38.3 MB (95.03%) - LeetHub

---
+++ reverse-bits/reverse-bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        
+        for(int i = 0; i < 32; i++){
+            res = res << 1;
+            if((n & 1) > 0){
+                res++;
+            }
+            n = n >> 1;
+        }
+        return res;
+    }
+}
